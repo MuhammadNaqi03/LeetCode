1227acc283095b6e84d3b68e5e3ef3344fda47f6
Muhammad Naqi <muhammad.naqi@g.bracu.ac.bd>
2021-01-10 17:19:25 +0000

    Solution for Palindrome Number

---
+++ Problem 9 - Palindrome Number/PalindromeNumberSolution.java
@@ -0,0 +1,13 @@
+class PalindromeNumberSolution {
+  public boolean isPalindrome(int x) {
+    if (x < 0) return false;
+    int reverse =0;
+    int original = x;
+    while(x != 0){
+      reverse = reverse*10 + x%10;
+      x /=10;
+    }
+
+    return reverse == original;
+  }
+}
