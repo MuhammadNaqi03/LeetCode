a49e1b2ad7e5929105eb7d86ae1e15d1dc2c5f56
Muhammad Naqi <muhammad.naqi@g.bracu.ac.bd>
2021-01-18 12:50:21 +0600

    Solution for Excel Sheet Column Title

---
+++ Problem 168 - Excel Sheet Column Title/ExcelColumSolution.java
@@ -0,0 +1,20 @@
+class Solution {
+  public String convertToTitle(int n) {
+    String title ="";
+    while(n!=0){
+      /*
+      * Instead of 1-26. lets convert it to 0 -25
+      * thus 0+65 = 65 will indicate as A, 66 as B 
+      * and so on.
+      * If we don't do that, say we are using title = (char)(n%26 + 64)
+      * Then this will work till Z appears.
+      * Because for Z, (char)(n%26 + 64) would be 64, which is @.
+      */
+            
+      title = (char)((n-1)%26 + 65) + title;
+      n = (n-1)/26;
+            
+    }
+    return title;
+  }
+}
