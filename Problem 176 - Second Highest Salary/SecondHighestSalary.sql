da765a698ab6e6cfce32f10a2cece12f0870aa85
Muhammad Naqi <muhammad.naqi@g.bracu.ac.bd>
2021-02-11 21:32:55 +0600

    Solution for Second Highest Salary

---
+++ Problem 176 - Second Highest Salary/SecondHighestSalary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+-- To check the value is null or not we wrap the query
+-- Have to use DISTINCT in case of duplication
+    IFNULL((SELECT DISTINCT Salary
+            FROM Employee
+            ORDER BY Salary DESC
+            LIMIT 1,1),NULL)
+            AS SecondHighestSalary
