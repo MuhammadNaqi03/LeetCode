9111a38b3417f03b2a770a2d7b504a0f6d3e68d1
Muhammad Naqi <muhammad.naqi@g.bracu.ac.bd>
2021-02-14 19:24:55 +0600

    Solution for Combine Two Tables

---
+++ Problem 175 - Combine Two Tables/CombineTwoTables.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT p.FirstName, p.LastName, a.City, a.State
+FROM Person p
+-- We have to consider that all name might not be
+-- associated with address
+-- So we need to find null value for those
+LEFT JOIN Address a
+ON p.PersonId = a.PersonId
