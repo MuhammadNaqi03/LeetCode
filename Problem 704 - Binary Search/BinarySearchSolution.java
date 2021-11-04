ee41bee7cdde2e43b34c15e0a991705771c5fc44
Muhammad Naqi <muhammad.naqi@g.bracu.ac.bd>
2021-11-04 13:44:11 +0600

    Solution for Binary Search

---
+++ Problem 704 - Binary Search/BinarySearchSolution.java
@@ -0,0 +1,14 @@
+class BinarySearchSolution {
+  public int search(int[] nums, int target) {
+      int mid;
+      int left = 0;
+      int right = nums.length-1;
+      while (left <= right) {
+          mid = (right + left)/2 ;
+          if (nums[mid] == target) return mid;
+          else if (nums[mid] > target) right = mid-1;
+          else left = mid+1;
+      }
+      return -1;
+  }
+}
