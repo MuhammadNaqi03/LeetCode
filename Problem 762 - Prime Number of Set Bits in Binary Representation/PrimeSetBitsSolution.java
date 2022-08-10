56e563832e7b848eb87586dc4fc6a975d35d77b9
Muhammad Naqi <muhammad.naqi@g.bracu.ac.bd>
2022-08-10 21:59:16 +0600

    Solution for Prime Number of Set Bits in Binary Representation

--- Problem 762 - Prime Number of Set Bits in Binary Representation/PrimeSetBitsSolution.java
+++ Problem 762 - Prime Number of Set Bits in Binary Representation/PrimeSetBitsSolution.java
@@ -28,5 +28,38 @@ class PrimeSetBitsSolution {
       }
      }
     return count;
+    
+    
+    
+    
+    
+    /**
+     * Approach 2: Using DP
+     * Firstly, we will have an arraylist with all the primes between 0 - 32 (say, primeNumberList) like approach 1.
+     * Afterwards, we will add all the numbers' bit count until right value inside an array.
+     * Then, will loop again from left to right and find out
+     * if the bit count of the number is present in the primeNumberList or not.
+     * Increment the prime number count if the list contains the bit count and return the count after the loop ends.
+     */
+    
+    // To know more about the approaches, check out:
+    // https://github.com/MuhammadNaqi03/LeetCode/blob/master/Problem%20338%20-%20Counting%20Bits/CountingBitsSolution.java
+    
+//     ArrayList<Integer> primeNumberList = new ArrayList<>(Arrays.asList(2,3,5,7,11,13,17,19,23,29,31));
+//     int [] bitCounter = new int [right + 1];
+//     bitCounter[0] = 0;
+//     bitCounter[1] = 1;
+    
+//     for (int i = 2; i<=right; i++) {
+//       bitCounter[i] = i % 2 == 0 ? bitCounter[i/2] : bitCounter[i/2] + 1;
+//     }
+ 
+//     int primeBitCount = 0;
+//     for (int i = left; i <= right; i++) {
+//       if(primeNumberList.contains(bitCounter[i])) {
+//         primeBitCount++;
+//       }
+//     }
+//     return primeBitCount;
   }
 }
